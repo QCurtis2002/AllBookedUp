3ef30b10af2289389c9ad1a98def507e0ea5b539
Quinn Curtis <qljcur2003@gmail.com>
2022-03-21 22:38:31 +0000

    Added DataContext and connection string

---
+++ AllBookedUp/Server/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using AllBookedUp.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllBookedUp.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
