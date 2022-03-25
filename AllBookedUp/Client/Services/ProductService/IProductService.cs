cf6846d188238d47f3a8818d07fd1bb24661edfb
Quinn Curtis <qljcur2003@gmail.com>
2022-03-25 22:39:03 +0000

    Add ProductService to the client

---
+++ AllBookedUp/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using AllBookedUp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllBookedUp.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task GetProduct();
+    }
+}
