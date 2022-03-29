3fa534b98220776b21225ba4db53088d08bad8ba
Quinn Curtis <qljcur2003@gmail.com>
2022-03-29 20:12:24 +0100

    Add Categories and more seeding

--- AllBookedUp/Shared/Product.cs
+++ AllBookedUp/Shared/Product.cs
@@ -13,7 +13,9 @@ namespace AllBookedUp.Shared
         public string Title { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
-        [Column(TypeName="decimal(18,2")]
+        [Column(TypeName = "decimal(18,2")]
         public decimal Price { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
     }
 }
