d5c27eb7c78a2ca6acbd21843846fd7082a02aff
Quinn Curtis <qljcur2003@gmail.com>
2022-03-30 22:55:26 +0100

    Added Pagination DTO

---
+++ AllBookedUp/Shared/ProductSearchResultDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllBookedUp.Shared
+{
+    public class ProductSearchResultDTO
+    {
+        public List<Product> Products { get; set; }
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
