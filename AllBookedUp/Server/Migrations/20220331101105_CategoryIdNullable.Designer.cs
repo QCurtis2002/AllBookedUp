221b17fe839870ef9e5188940a3ab230316122a1
Quinn Curtis <qljcur2003@gmail.com>
2022-03-31 11:38:03 +0100

    Change CategoryId to Nullable

--- AllBookedUp/Server/Migrations/20220331101105_CategoryIdNullable.Designer.cs
+++ AllBookedUp/Server/Migrations/20220331101105_CategoryIdNullable.Designer.cs
@@ -4,14 +4,16 @@ using AllBookedUp.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AllBookedUp.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220331101105_CategoryIdNullable")]
+    partial class CategoryIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
