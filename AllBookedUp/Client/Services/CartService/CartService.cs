5dd0beaedf6538e74eade6c4534d8c4208c41cce
Quinn Curtis <qljcur2003@gmail.com>
2022-04-05 16:02:39 +0100

    Added Cart Counter and Services

---
+++ AllBookedUp/Client/Services/CartService/CartService.cs
@@ -0,0 +1,47 @@
+﻿using AllBookedUp.Client.Services.ProductService;
+using AllBookedUp.Shared;
+using Blazored.LocalStorage;
+using Blazored.Toast.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllBookedUp.Client.Services.CartService
+{
+    public class CartService : ICartService
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly IToastService _toastService;
+        private readonly IProductService _productService;
+
+        public event Action OnChange;
+
+        public CartService(ILocalStorageService localStorage,
+            IToastService toastService,
+            IProductService productService)
+        {
+            _localStorage = localStorage;
+            _toastService = toastService;
+            _productService = productService;
+        }
+
+        public async Task AddToCart(Product product)
+        {
+            var cart = await _localStorage.GetItemAsync<List<Product>>("cart");
+            if (cart == null)
+            {
+                cart = new List<Product>();
+            }
+            cart.Add(product);
+            await _localStorage.SetItemAsync("cart", cart);
+
+            var prod = await _productService.GetProductById(product.Id);
+            //below line may be wrong
+            _toastService.ShowSuccess(product.Title, "Added to cart:");
+
+            OnChange.Invoke();
+
+        }
+    }
+}
---
+++ AllBookedUp/Client/Services/CartService/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using AllBookedUp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllBookedUp.Client.Services.CartService
+{
+    public interface ICartService
+    {
+        event Action OnChange;
+        Task AddToCart(Product product);
+    }
+}
