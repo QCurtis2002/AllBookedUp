95c7eed7849baafb6abe0ece771444b786da7915
Quinn Curtis <qljcur2003@gmail.com>
2022-04-07 23:03:28 +0100

    Added Migration (Changed Category names)

---
+++ AllBookedUp/Server/Migrations/20220407125425_ChangeCategories.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AllBookedUp.Server.Migrations
+{
+    public partial class ChangeCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Action", "action" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Classics", "classics" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Mystery", "mystery" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Books", "books" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Movies", "movies" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Name", "Url" },
+                values: new object[] { "Video Games", "video-games" });
+        }
+    }
+}
